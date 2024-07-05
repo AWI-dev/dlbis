5b87eeec6b8dd2681e43526617e64ab6dcfd2ceb
Francisco L. Espina II <105304650+Franzespina21@users.noreply.github.com>
2024-07-05 15:36:20 +0800

    Update useApiRequest.ts

--- src/hooks/useApiRequest.ts
+++ src/hooks/useApiRequest.ts
@@ -9,49 +9,41 @@ type RequestHeaders = Record<string, string>;
 
 const { getCookie } = useCookie();
 const { decryptData } = useEncryption(SHARED_KEY);
-// const csrfTokenMeta = document.querySelector('meta[name="csrf-token"]');
-// let csrf_token: string | null = csrfTokenMeta ? csrfTokenMeta.getAttribute('content') : null;
-const csrfToken = (window as any).Laravel.csrfToken;
-console.log(csrfToken);
+
 
 async function fetchApi(
   api: string | null,
   urlPath: string,
   requestData: RequestData,
   method: RequestMethod,
-  headers: RequestHeaders = { 'Content-Type': 'application/json' },
+  headers: RequestHeaders = { 'Content-Type': 'application/json'}
 ): Promise<any> {
   const cookieValue = getCookie('rrf');
-  let accessToken: string | null = null;
+  let accessToken:any = null;
   
   if (cookieValue) {
     accessToken = decryptData(cookieValue);
   }
-  
   const requestOptions: RequestInit = {
     method,
-    headers: {
+    headers :{
       ...headers,
-      'Authorization': accessToken ? `Bearer ${accessToken}` : '',
-      'X-CSRF-TOKEN': csrfToken || '',
+      'Authorization': `Bearer ${accessToken}`
     },
-    body: method === 'POST' || method === 'PUT' ? JSON.stringify(requestData) : undefined,
+    body: method === 'POST' ? JSON.stringify(requestData) : undefined,
   };
-  
   try {
     let endpoint = API_BASE_URL + urlPath;
-    if (api) {
+    if(api){
       endpoint = api + urlPath;
     }
+  
     
-    const response = await fetch(endpoint, requestOptions);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
+    const response = await fetch(endpoint, requestOptions as RequestInit);
     return await response.json();
-  } catch (err: any) {
+  } catch (err:any) {
     console.error('Error caught:', err.message);
-    err.code = 'Failed';
+    err['code'] = 'Failed';
     throw err;
   }
 }
